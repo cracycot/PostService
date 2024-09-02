bef1d4da304239c985414a84f4dc603445f47b89
cracycot <cracycot@mail.ru>
2024-09-02 21:15:52 +0300

    MADE KafkaConsumer

---
+++ src/main/java/org/example/services/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package org.example.services;
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    @KafkaListener(topics = "posts", groupId = "1")
+    public void listen(ConsumerRecord<Long, String> record) {
+        System.out.println("recieved messeg" + record.value());
+    }
+
+}
