8c1885947c9c25db54deb93b1071ee0480975fc4
cracycot <cracycot@mail.ru>
2024-09-04 00:17:35 +0300

    MADE PostElastic entity

---
+++ src/main/java/org/example/models/PostElastic.java
@@ -0,0 +1,49 @@
+package org.example.models;
+
+import jakarta.persistence.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "posts")
+public class PostElastic {
+    @Id
+    private Long id;
+    @Field(type = FieldType.Text, name = "title")
+    private String title;
+    @Field(type = FieldType.Text, name = "content")
+    private String content;
+
+    @Override
+    public String toString() {
+        return "PostElastic{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
