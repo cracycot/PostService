6e66182f13e9c88bd1ee5f3a6dafea85627e7825
cracycot <cracycot@mail.ru>
2024-12-14 19:04:34 +0300

    feat: made image

---
+++ src/main/java/org/example/models/Image.java
@@ -0,0 +1,39 @@
+package org.example.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String s3url;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setS3url(String s3url) {
+        this.s3url = s3url;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getS3url() {
+        return s3url;
+    }
+}
