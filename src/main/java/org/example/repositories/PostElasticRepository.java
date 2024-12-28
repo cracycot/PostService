833f2cc83df91f45be14e5fff1be683692b49f7a
cracycot <cracycot@mail.ru>
2024-12-28 15:28:12 +0300

    fix: repo

--- src/main/java/org/example/repositories/PostElasticRepository.java
+++ src/main/java/org/example/repositories/PostElasticRepository.java
@@ -5,12 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import java.util.List;
-
 public interface PostElasticRepository extends ElasticsearchRepository<PostElastic, Long> {
-//    List<PostElastic> findByTitleContaining(String title); // надо сдлелать pagebale
-//    List<PostElastic> findByContentContaining(String content); //как обьединить
     Page<PostElastic> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
-//    Page<PostElastic> findByTitleContaining(String title, Pageable pageable);
-
 }
--- src/main/java/org/example/repositories/PostRepository.java
+++ src/main/java/org/example/repositories/PostRepository.java
@@ -1,6 +1,5 @@
 package org.example.repositories;
 
-import org.aspectj.apache.bcel.generic.LOOKUPSWITCH;
 import org.example.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
