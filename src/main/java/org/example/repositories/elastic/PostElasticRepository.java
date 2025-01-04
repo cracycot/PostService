9aac05388b788dc8f3d5e6dbf84b00b6bb7324d8
cracycot <cracycot@mail.ru>
2025-01-04 21:37:57 +0300

    feat: update postElasticRepo

--- src/main/java/org/example/repositories/elastic/PostElasticRepository.java
+++ src/main/java/org/example/repositories/elastic/PostElasticRepository.java
@@ -1,12 +1,10 @@
-package org.example.repositories;
+package org.example.repositories.elastic;
 
-import org.example.models.Post;
+import org.example.DTO.PostElasticDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository("postElasticRepository")
-public interface PostElasticRepository extends ElasticsearchRepository<Post, Long> {
-    Page<Post> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
+public interface PostElasticRepository extends ElasticsearchRepository<PostElasticDTO, String> {
+    Page<PostElasticDTO> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 }
