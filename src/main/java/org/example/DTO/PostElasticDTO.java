6f153d888013a91b5880b9156db0f4c3dd699674
cracycot <cracycot@mail.ru>
2025-01-04 21:33:26 +0300

    feat: made DTO to work with elasticSearch

---
+++ src/main/java/org/example/DTO/PostElasticDTO.java
@@ -0,0 +1,67 @@
+package org.example.DTO;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(indexName = "posts")
+public class PostElasticDTO {
+
+    @Field(type = FieldType.Text)
+    private String id;
+
+    @Field(type = FieldType.Long)
+    private Long idOwner;
+
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String content;
+
+    @Field(type = FieldType.Text)
+    private List<String> urls = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getIdOwner() {
+        return idOwner;
+    }
+
+    public void setIdOwner(Long idOwner) {
+        this.idOwner = idOwner;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<String> getUrls() {
+        return urls;
+    }
+
+    public void setUrls(List<String> Urls) {
+        this.urls = urls;
+    }
+}
