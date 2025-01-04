1258a2e1096502c0307722220827d6048ecc16fe
cracycot <cracycot@mail.ru>
2025-01-04 21:38:22 +0300

    feat: add findByIdWithImages

--- src/main/java/org/example/repositories/jpa/PostJpaRepository.java
+++ src/main/java/org/example/repositories/jpa/PostJpaRepository.java
@@ -1,9 +1,15 @@
-package org.example.repositories;
+package org.example.repositories.jpa;
 
 import org.example.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository("postJpaRepository")
 public interface PostJpaRepository extends JpaRepository<Post, Long> {
+    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.images WHERE p.id = :id")
+    Optional<Post> findByIdWithImages(@Param("id") Long id);
 }
